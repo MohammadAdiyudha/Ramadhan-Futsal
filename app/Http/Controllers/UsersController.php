efe7b63b1e63753c7ceacbe2067d82569454d0ca
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2023-03-06 13:14:18 +0700

    Add function for edit user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -13,4 +14,22 @@ class UsersController extends Controller
         $akun_user = DB::select('select * from users');
         return view('admin/dataUser',['users'=>$akun_user]);
      }
+
+     public function edit($id)
+    {
+        $user   = User::whereId($id)->first();
+        return view('admin/editUser')->with('user', $user);
+        // return view('admin/editUser', compact('user'));
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->update();
+
+        return redirect('admin/data_user')->with('success','Ubah data berhasil');
+    }
 }

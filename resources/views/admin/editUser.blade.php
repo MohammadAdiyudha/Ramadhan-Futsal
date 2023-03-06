6f12e65ff8b11c62855e1fa867ca7ed66de2b2eb
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2023-03-06 16:58:35 +0700

    Fix Layout Form

--- resources/views/admin/editUser.blade.php
+++ resources/views/admin/editUser.blade.php
@@ -10,34 +10,46 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-7">
 
                 <div class="card">
                     <div class="card-body">
 
                         <form action="{{ url('admin/update-user/'.$user->id) }}" method="POST">
                             @csrf
-                            <div class="form-group mb-3">
-                                <label for="">Name</label>
-                                <input type="text" name="name" value="{{$user->name}}" class="form-control">
+                            {{-- Kolom Nama --}}
+                            <div class="form-group row mb-3">
+                                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="name" id="name" value="{{$user->name}}" class="form-control">
+                                </div>
                             </div>
-                            <div class="form-group mb-3">
-                                <label for="">Email</label>
-                                <input type="text" name="email" value="{{$user->email}}" class="form-control">
+                            {{-- Kolom Email --}}
+                            <div class="form-group row mb-3">
+                                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="email" id="email" value="{{$user->email}}" class="form-control">
+                                </div>
                             </div>
-                            <div class="form-group mb-3">
-                                <label for="">Role</label>
-                                <select name="role">
-                                    <option value=1 <?php echo $user['is_admin'] == 1 ? ' selected ' : '';?>>Admin</option>
-                                    <option value=0 <?php echo $user['is_admin'] == 0 ? ' selected ' : '';?>>Pelanggan</option>
-                                </select>
+                            {{-- Kolom Role --}}
+                            <div class="form-group row mb-3">
+                                <label for="role" class="col-sm-2 col-form-label">Role</label>
+                                <div class="col-sm-10">
+                                    <select name="role" id="role" class="form-select col-sm-12">
+                                        <option value=1 <?php echo $user['is_admin'] == 1 ? ' selected ' : '';?>>Admin</option>
+                                        <option value=0 <?php echo $user['is_admin'] == 0 ? ' selected ' : '';?>>Pelanggan</option>
+                                    </select>
+                                </div>
                             </div>
+                            {{-- Kolom Verifikasi --}}
+                            {{-- Dengan Kondisi Belum Verifikasi --}}
                             @if ($user->email_verified_at == NULL)
-                                <div class="form-group mb-3">
-                                    <input type="checkbox" name="verifikasi" value="a" class="form-control">
-                                    <label for="">Verifikasi Email</label>
+                                <div class="form-check mb-3">
+                                    <input type="checkbox" name="verifikasi" id="verifikasi" value="a" class="form-check-input">
+                                    <label for="verifikasi" class="form-check-label">Verifikasi Email</label>
                                 </div>
                             @endif
+                            {{-- Tombol Submit --}}
                             <div class="form-group mb-3">
                                 <button type="submit" class="btn btn-primary">Update User</button>
                             </div>

86c7cb8cf0196a4374f0e3b978cf926d137ae4a5
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2023-03-10 15:38:17 +0700

    Add middleware auth

--- app/Http/Controllers/ReservasiController.php
+++ app/Http/Controllers/ReservasiController.php
@@ -9,6 +9,12 @@ use DB;
 
 class ReservasiController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+    }
+
     // Tampil Reservasi Sisi USER
     public function indexUser() {
         $user_table = Auth::user();
@@ -63,5 +69,31 @@ class ReservasiController extends Controller
     public function hapus($reservasi_id){
         DB::table('reservasis')->where('reservasi_id', $reservasi_id)->delete();
         return redirect()->back()->with('success','Hapus data berhasil');
+    }
+
+    public function edit($id)
+    {
+        $reservasi   = DB::table('reservasis')
+                        ->where('reservasi_id','=',$id)
+                        ->first();
+        return view('editReservasi')->with('reservasi', $reservasi);
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $reservasi = Reservasi::find($id);
+
+        $reservasi->no_hp = $request->input('no_hp');
+        $reservasi->tanggal = $request->input('tanggal');
+        $reservasi->jam_awal = $request->input('jam_awal');
+        $reservasi->jam_akhir = $request->input('jam_akhir');
+        $reservasi->durasi = $request->input('durasi');
+        $reservasi->harga = $request->input('harga');
+        $reservasi->status = "Pending";
+        $reservasi->update();
+
+        return redirect("data-reservasi")->with('success','Ubah data berhasil');
     }
 }

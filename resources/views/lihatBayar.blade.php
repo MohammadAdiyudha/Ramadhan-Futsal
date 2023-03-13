e11d4f2186c4cd4af8ecb1097e668ee88b6bc3f5
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2023-03-13 12:04:55 +0700

    Tampil img sesuai database

--- resources/views/lihatBayar.blade.php
+++ resources/views/lihatBayar.blade.php
@@ -1,58 +1,52 @@
 @extends('adminlte::page')
 
-@section('title', 'Buat Reservasi')
+@section('title', 'Lihat Bukti Bayar')
 
 @section('content_header')
-    <h1>Buat Reservasi</h1>
+    <h1>Lihat Bukti Bayar</h1>
 @stop
 
 @section('content')
     <blockquote class="quote-olivia">
         <h5 class="text-olivia">Tips!</h5>
-        <p>Pastikan jadwal tidak bentrok dengan jadwal lain.</p>
-        <p>
-            Tekan tombol <strong>Hitung Harga</strong> sebelum menekan tombol <strong>Submit</strong>
-            untuk melihat total yang perlu dibayar.
-        </p>
+        <p>Pastikan pembayaran sudah diterima.</p>
     </blockquote>
     <div class="container">
-        <div class="row justify">
-            <div class="col-md-8">
+        <div class="row">
+            <div class="col">
+                <img src="{{asset('assets/buktiBayar/'.$pembayaran->bukti_bayar)}}" alt="bukti bayar" class="img-fluid">
+            </div>
+            <div class="col">
+                <div class="row justify">
+                    <div class="col">
+                        @if (session('status'))
+                            <h6 class="alert alert-success">{{ session('status') }}</h6>
+                        @endif
+                        <div class="card">
+                            <div class="card-body">
+                                {{-- BUAT DEBUG --}}
+                                {{-- {{$reservasi->reservasi_id}} --}}
 
-                @if (session('status'))
-                    <h6 class="alert alert-success">{{ session('status') }}</h6>
-                @endif
-
-                <div class="card">
-                    <div class="card-body">
-                        {{-- BUAT DEBUG --}}
-                        {{-- {{$reservasi->reservasi_id}} --}}
-
-                            {{-- Buat cek id aktif bisa atau tidak --}}
-                            {{-- <p>{{Auth::user()->id}}</p> --}}
-                            <div class="form-group row mb-4">
-                                <label for="" class="col-sm-2 col-form-label">Bayar ID</label>
-                                <input type="text" name="bayar_id" class="form-control col-sm-10" value="{{$pembayaran->bayar_id}}" readonly>
+                                    {{-- Buat cek id aktif bisa atau tidak --}}
+                                    {{-- <p>{{Auth::user()->id}}</p> --}}
+                                    <div class="form-group col mb-4">
+                                        <label for="" class="col-form-label">Bayar ID</label>
+                                        <input type="text" name="bayar_id" class="form-control" value="{{$pembayaran->bayar_id}}" readonly>
+                                    </div>
+                                    <div class="form-group col mb-4">
+                                        <label for="" class="col-form-label">Reservasi ID</label>
+                                        <input type="text" name="reservasi_id" class="form-control" value="{{$pembayaran->reservasi_id}}" readonly>
+                                    </div>
+                                    <div class="form-group col mb-4">
+                                        <label for="" class="col-form-label">Atas Nama</label>
+                                        <input type="text" name="atas_nama" class="form-control" value="{{$pembayaran->atas_nama}}" readonly>
+                                    </div>
+                                    <div class="form-group col mb-4">
+                                        <label for="" class="col-form-label">Jenis Bayar</label>
+                                        <input type="text" name="jenis_bayar" class="form-control" value="{{$pembayaran->jenis_bayar}}" readonly>
+                                    </div>
                             </div>
-                            <div class="form-group row mb-4">
-                                <label for="" class="col-sm-2 col-form-label">Reservasi ID</label>
-                                <input type="text" name="reservasi_id" class="form-control col-sm-10" value="{{$pembayaran->reservasi_id}}" readonly>
-                            </div>
-                            <div class="form-group row mb-4">
-                                <label for="" class="col-sm-2 col-form-label">Atas Nama</label>
-                                <input type="text" name="atas_nama" class="form-control col-sm-10" value="{{$pembayaran->atas_nama}}" readonly>
-                            </div>
-                            <div class="form-group row mb-4">
-                                <label for="" class="col-sm-2 col-form-label">Jenis Bayar</label>
-                                <input type="text" name="jenis_bayar" class="form-control col-sm-10" value="{{$pembayaran->jenis_bayar}}" readonly>
-                            </div>
-                            <div class="form-group row mb-4">
-                                <label for="" class="col-sm-2 col-form-label">Bukti Bayar</label>
-                                <input type="text" name="bukti_bayar" class="form-control col-sm-10" value="{{$pembayaran->bukti_bayar}}" readonly>
-                            </div>
-
-
-
+                        </div>
                     </div>
                 </div>
             </div>

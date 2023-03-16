a338b61be8f452d16bf694979c0d48e50ca30934
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2023-03-16 15:23:18 +0700

    Algoritma anti double booking untuk buat dan edit

--- app/Http/Controllers/ReservasiController.php
+++ app/Http/Controllers/ReservasiController.php
@@ -9,6 +9,8 @@ use App\Models\Pembayaran;
 use DB;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+
 
 class ReservasiController extends Controller
 {
@@ -60,16 +62,45 @@ class ReservasiController extends Controller
             if ($validator->fails()) {
                 return redirect()->back()->withErrors($validator);
             };
-            $reservasi->user_id = $user->id;
-            $reservasi->no_hp = $request->input('no_hp');
-            $reservasi->tanggal = $request->input('tanggal');
-            $reservasi->jam_awal = $request->input('jam_awal');
-            $reservasi->jam_akhir = $request->input('jam_akhir');
-            $reservasi->durasi = $request->input('durasi');
-            $reservasi->harga = $request->input('harga');
-            $reservasi->status = "Pending";
-            $reservasi->save();
-            return redirect()->back()->with('success','Reservasi Berhasil Dibuat');
+
+            // Untuk Double Booking
+            // Tambah * Kurang 1 Detik
+            $parseJamAwal = Carbon::createFromFormat('H:i:s',$request->input('jam_awal'));
+            $newJamAwal = $parseJamAwal->addSecond()->toTimeString();
+            $parseJamAkhir = Carbon::createFromFormat('H:i:s',$request->input('jam_akhir'));
+            $newJamAkhir = $parseJamAkhir->subSecond()->toTimeString();
+            // dd($newJamAwal,$newJamAkhir);
+
+            // Algoritma Duble Booking / Jadwal Bentrok
+            $cekBooked = Reservasi::where('tanggal', $request->input('tanggal'))
+                                    ->where(
+                                        fn ($q) => $q->whereBetween('jam_awal', [$newJamAwal, $newJamAkhir])
+                                                    ->orWhereBetween('jam_akhir', [$newJamAwal, $newJamAkhir])
+                                                    ->orWhere (
+                                                        fn ($q) => $q->where('jam_awal', '<', $newJamAwal)
+                                                                    ->where('jam_akhir', '>', $newJamAkhir)
+                                                    )
+                                    )
+                                    ->exists();
+            // Debug
+            // dd($newJamAwal,$newJamAkhir,$cekBooked);
+
+            // Jika ada jadwal bentrok
+            if ($cekBooked) {
+                return redirect()->back()->with('error','JADWAL BENTROK!!! Ganti ke jadwal lain');
+            // Jika jadwal tidak bentrok
+            } else {
+                $reservasi->user_id = $user->id;
+                $reservasi->no_hp = $request->input('no_hp');
+                $reservasi->tanggal = $request->input('tanggal');
+                $reservasi->jam_awal = $request->input('jam_awal');
+                $reservasi->jam_akhir = $request->input('jam_akhir');
+                $reservasi->durasi = $request->input('durasi');
+                $reservasi->harga = $request->input('harga');
+                $reservasi->status = "Pending";
+                $reservasi->save();
+                return redirect()->back()->with('success','Reservasi Berhasil Dibuat');
+            }
 
         } catch(\Illuminate\Database\QueryException $e){
             // Handling error duplicate
@@ -107,6 +138,20 @@ class ReservasiController extends Controller
     {
 
         $reservasi = Reservasi::find($id);
+        // OLD DATA - TEMPORARY
+        $oldJamAwal = $reservasi->jam_awal;
+        $oldJamAkhir = $reservasi->jam_akhir;
+        $oldDurasi = $reservasi->durasi;
+        $oldHarga = $reservasi->harga;
+
+        // WAJIB - jika edit tidak ada perubahan
+        // Set NULL agar tidak error saat tidak ada perubahan
+        $reservasi->jam_awal = NULL;
+        $reservasi->jam_akhir = NULL;
+        $reservasi->durasi = NULL;
+        $reservasi->harga = NULL;
+        $reservasi->update();
+
         $validator = Validator::make($request->all(), [
             'no_hp'  => 'required|min:10',
             'tanggal'  => 'required',
@@ -118,16 +163,49 @@ class ReservasiController extends Controller
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator);
         };
-        $reservasi->no_hp = $request->input('no_hp');
-        $reservasi->tanggal = $request->input('tanggal');
-        $reservasi->jam_awal = $request->input('jam_awal');
-        $reservasi->jam_akhir = $request->input('jam_akhir');
-        $reservasi->durasi = $request->input('durasi');
-        $reservasi->harga = $request->input('harga');
-        $reservasi->status = "Pending";
-        $reservasi->update();
 
-        return redirect("data-reservasi")->with('success','Ubah data berhasil');
+        // Untuk Double Booking
+        // Tambah * Kurang 1 Detik
+        $parseJamAwal = Carbon::createFromFormat('H:i:s',$request->input('jam_awal'));
+        $newJamAwal = $parseJamAwal->addSecond()->toTimeString();
+        $parseJamAkhir = Carbon::createFromFormat('H:i:s',$request->input('jam_akhir'));
+        $newJamAkhir = $parseJamAkhir->subSecond()->toTimeString();
+        // dd($newJamAwal,$newJamAkhir);
+
+        // Algoritma Duble Booking / Jadwal Bentrok
+        $cekBooked = Reservasi::where('tanggal', $request->input('tanggal'))
+                                ->where(
+                                    fn ($q) => $q->whereBetween('jam_awal', [$newJamAwal, $newJamAkhir])
+                                                ->orWhereBetween('jam_akhir', [$newJamAwal, $newJamAkhir])
+                                                ->orWhere (
+                                                    fn ($q) => $q->where('jam_awal', '<', $newJamAwal)
+                                                                ->where('jam_akhir', '>', $newJamAkhir)
+                                                )
+                                )
+                                ->exists();
+        // Jika ada jadwal bentrok
+        if ($cekBooked) {
+            // Data dikembalikan ke old data
+            $reservasi->jam_awal = $oldJamAwal;
+            $reservasi->jam_akhir = $oldJamAkhir;
+            $reservasi->durasi = $oldDurasi;
+            $reservasi->harga = $oldHarga;
+            $reservasi->update();
+            return redirect()->back()->with('error','JADWAL BENTROK!!! Ganti ke jadwal lain');
+        // Jika jadwal tidak bentrok
+        } else {
+            $reservasi->no_hp = $request->input('no_hp');
+            $reservasi->tanggal = $request->input('tanggal');
+            $reservasi->jam_awal = $request->input('jam_awal');
+            $reservasi->jam_akhir = $request->input('jam_akhir');
+            $reservasi->durasi = $request->input('durasi');
+            $reservasi->harga = $request->input('harga');
+            $reservasi->status = "Pending";
+            $reservasi->update();
+            return redirect("data-reservasi")->with('success','Ubah data berhasil');
+        }
+
+
     }
 
     public function setStatus(Request $request)
